36436ace7589fff000d1a98bce783cfde0390143
Mohammad Nabi Noorzai <mnoorzai21@gmail.com>
2022-05-13 21:13:29 -0700

    created database and tables in schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+drop database if exist employee_db;
+create database employee_db;
+
+use employee_db;
+
+create table department (
+    id int auto_increment not null primary key,
+    department_name varchar(30) not null
+);
+
+create table role (
+    id int auto_increment not null primary key,
+    title varchar(30) not null,
+    salary decimal not null,
+    department_id int not,
+    foreign key (department_id)
+        references department(id)
+);
+
+create table employee (
+    id int auto_increment not null primary key,
+    first_name varchar(30) not null,
+    last_name varchar(30) not null,
+    role_id int,
+    manager_id int,
+    foreign key (role_id)
+        references role(id),
+    foreign key (manager_id)
+        references employee(id)
+);
